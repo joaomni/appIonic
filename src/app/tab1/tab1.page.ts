134a3b0a59408cb60b2a5e49aa7d86d7eb4da014
joaoqsl <joaojpmoreira25@gmail.com>
2019-11-08 17:44:34 -0300

    Função para listar todos calculos recentes

--- src/app/tab1/tab1.page.ts
+++ src/app/tab1/tab1.page.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { PesoHomemService } from './services/peso-homem.service';
+import { convertUpdateArguments } from '@angular/compiler/src/compiler_util/expression_converter';
 
 @Component({
   selector: 'app-tab1',
@@ -10,11 +11,27 @@ export class Tab1Page {
 
   private altura:string;
   private resultado:number;
+  private allOld:number;
 
   constructor(private peso: PesoHomemService) {}
 
   calcula(){
     this.resultado = this.peso.calcularPeso(parseFloat(this.altura));
+
+
+
+    var sliding = document.createElement('ion-item-sliding');
+    var item = document.createElement('ion-item');
+    var label = document.createElement('ion-label');
+    var text = document.createTextNode(String(this.resultado));
+    label.appendChild(text);
+    item.appendChild(label);
+    sliding.appendChild(item);
+
+    var tudo = document.querySelector('#lista');
+
+    tudo.appendChild(sliding);
+    
   }
 
   exibeResultado(){
